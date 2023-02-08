c1071c718b2674575d45538a1708ac0b14ed5256
schons <schonsluuiz@gmail.com>
2023-02-07 22:30:51 -0300

    First API with FastAPi

---
+++ Intro/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Server is running"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
